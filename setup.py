e65f80af7cf4e7d1f20bf48847441188ba85364f
redzej <redzej.erwin@gmail.com>
2017-09-03 21:21:28 +0200

    Add PyPI configuration

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='graphene-permissions',
+    packages=['graphene-permissions'],
+    license='MIT',
+    version='0.1.0',
+    description='Simple graphene-django permission system',
+    author='Erwin Redzej',
+    keywords=['graphene', 'django', 'permissions'],
+    classifiers=[],
+)
