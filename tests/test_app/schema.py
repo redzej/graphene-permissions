379dcadffff15cf2cdc74432c35d09eb7d2e2500
redzej <redzej@users.noreply.github.com>
2018-01-26 09:36:12 -0800

    Extend test schema

--- tests/test_app/schema.py
+++ tests/test_app/schema.py
@@ -7,16 +7,16 @@ from graphene_permissions.permissions import AllowAny, AllowStaff
 from tests.test_app.models import User, Pet
 
 
-class RestrictedUserNode(AuthNode, DjangoObjectType):
+class UserNode(AuthNode, DjangoObjectType):
     permission_classes = (AllowStaff,)
 
     class Meta:
         model = User
-        filter_fields = ('first_name',)
+        filter_fields = ('username',)
         interfaces = (relay.Node,)
 
 
-class RestrictedPetNode(AuthNode, DjangoObjectType):
+class PetNode(AuthNode, DjangoObjectType):
     permission_classes = (AllowStaff,)
 
     class Meta:
@@ -25,22 +25,21 @@ class RestrictedPetNode(AuthNode, DjangoObjectType):
         interfaces = (relay.Node,)
 
 
-class NormalPetNode(AuthNode, DjangoObjectType):
+class OwnedPetNode(AuthNode, DjangoObjectType):
     permission_classes = (AllowAny,)
 
     class Meta:
         model = Pet
         filter_fields = ('name',)
-        exclude_fields = ('id', 'User')
         interfaces = (relay.Node,)
 
 
-class NormalUserNode(AuthNode, DjangoObjectType):
+class InfoPetNode(AuthNode, DjangoObjectType):
     permission_classes = (AllowAny,)
 
     class Meta:
         model = Pet
-        exclude_fields = ('id', 'pet_set')
+        exclude_fields = ('owner',)
         filter_fields = ('name',)
         interfaces = (relay.Node,)
 
@@ -50,15 +49,16 @@ class StaffRequiredFilter(AuthFilter):
 
 
 class PetsQuery:
-    restricted_pet = graphene.Field(RestrictedPetNode)
-    restricted_user = graphene.Field(RestrictedUserNode)
-    all_restricted_pets = StaffRequiredFilter(RestrictedPetNode)
-    all_restricted_users = StaffRequiredFilter(RestrictedUserNode)
+    pet = graphene.Field(PetNode)
+    user = graphene.Field(UserNode)
+    all_pets = StaffRequiredFilter(PetNode)
+    all_users = StaffRequiredFilter(UserNode)
 
-    pet = graphene.Field(NormalPetNode)
-    user = graphene.Field(NormalUserNode)
-    all_pets = AuthFilter(NormalPetNode)
-    all_users = AuthFilter(NormalUserNode)
+    my_pet = graphene.Field(OwnedPetNode)
+    my_all_pets = AuthFilter(PetNode)
+
+    other_pet = graphene.Field(InfoPetNode)
+    other_pets = AuthFilter(InfoPetNode)
 
 
 class Query(PetsQuery, ObjectType):
