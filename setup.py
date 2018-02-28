7bf3a135d702d21cc76b00fdf7982391d1dd039c
redzej <redzej@users.noreply.github.com>
2018-02-28 15:55:42 -0800

    Fix setup.py

--- setup.py
+++ setup.py
@@ -6,12 +6,10 @@ from pip.req import parse_requirements
 from graphene_permissions import __version__
 from setuptools import setup
 
-here = path.abspath(path.dirname(__file__))
+base_dir = path.abspath(path.dirname(__file__))
 
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
-
-install_requirements = parse_requirements('requirements/requirements.txt',  session=PipSession())
+requirements_path = path.join(base_dir, 'requirements', 'requirements.txt')
+install_requirements = parse_requirements(requirements_path, session=PipSession())
 
 setup(
     name='graphene-permissions',
@@ -19,8 +17,8 @@ setup(
     license='MIT',
     version=__version__,
     author='redzej',
-    description='Simple graphene-django permission system',
-    long_description=long_description,
+    description='Simple graphene-django permission system.',
+    long_description=open(path.join(base_dir, 'README.md'), 'r', encoding='utf-8').read(),
     url='https://github.com/redzej/graphene-permissions',
     install_requires=[str(ir.req) for ir in install_requirements],
     keywords='graphene django permissions permission system',
