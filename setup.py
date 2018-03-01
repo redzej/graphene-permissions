776eb2494aff58272fd471481cd2cc80f57bd3e1
redzej <redzej@users.noreply.github.com>
2018-02-28 17:20:23 -0800

    Minor fixes and isort

--- setup.py
+++ setup.py
@@ -1,15 +1,12 @@
-from os import path
-
+from graphene_permissions import __version__
 from pip.download import PipSession
 from pip.req import parse_requirements
-
-from graphene_permissions import __version__
 from setuptools import setup
 
-base_dir = path.abspath(path.dirname(__file__))
+with open('README.md', encoding="utf-8") as f:
+    long_description = f.read()
 
-requirements_path = path.join(base_dir, 'requirements', 'requirements.txt')
-install_requirements = parse_requirements(requirements_path, session=PipSession())
+install_requirements = parse_requirements('requirements/requirements.txt', session=PipSession())
 
 setup(
     name='graphene-permissions',
@@ -18,7 +15,7 @@ setup(
     version=__version__,
     author='redzej',
     description='Simple graphene-django permission system.',
-    long_description=open(path.join(base_dir, 'README.md'), 'r', encoding='utf-8').read(),
+    long_description=long_description,
     url='https://github.com/redzej/graphene-permissions',
     install_requires=[str(ir.req) for ir in install_requirements],
     keywords='graphene django permissions permission system',
