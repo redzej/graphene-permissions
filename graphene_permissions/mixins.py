969f390b56f3ba25d71348fdda621fbd574c7045
redzej <25885397+redzej@users.noreply.github.com>
2021-01-24 03:21:11 +0100

    Filter only when has_permission (#32)
    No need to create a (potentially complex) filtered queryset if permission is not granted.
    
    Co-authored-by: Cyrille Pontvieux <cyrille@enialis.net>

--- graphene_permissions/mixins.py
+++ graphene_permissions/mixins.py
@@ -74,22 +74,16 @@ class AuthFilterPre270(DjangoFilterConnectionField):
         **args
     ):
 
+        if not cls.has_permission(info):
+            return super(DjangoFilterConnectionField, cls).connection_resolver(
+                resolver, connection, default_manager.none(), max_limit, enforce_first_or_last,
+                root, info, **args,
+            )
+
         filter_kwargs = {k: v for k, v in args.items() if k in filtering_args}
         qs = filterset_class(
             data=filter_kwargs, queryset=default_manager.get_queryset()
         ).qs
-
-        if not cls.has_permission(info):
-            return super(DjangoFilterConnectionField, cls).connection_resolver(
-                resolver,
-                connection,
-                qs.none(),
-                max_limit,
-                enforce_first_or_last,
-                root,
-                info,
-                **args,
-            )
 
         return super(DjangoFilterConnectionField, cls).connection_resolver(
             resolver,
